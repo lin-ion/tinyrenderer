5df780dd42c5b218f37731ca4c56df4d5087d7f6
lin-ion <lin-ion@naver.com>
2024-08-21 21:37:53 +0900

    Second attempt

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
-void line_parametric(const int x0, const int y0, const int x1, const int y1,
-                     TGAImage &image, const TGAColor &color) {
-  for (float t = 0.; t < 1.; t += .1) {
-    const int x = x0 + t * (x1 - x0);
-    const int y = y0 + t * (y1 - y0);
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+  for (int x = x0; x <= x1; x++) {
+    float t = (x - x0) / (float)(x1 - x0);
+    int y = y0 * (1. - t) + y1 * t;
     image.set(x, y, color);
   }
 }
@@ -15,10 +14,11 @@ void line_parametric(const int x0, const int y0, const int x1, const int y1,
 int main(int argc, char **argv) {
   TGAImage image(100, 100, TGAImage::RGB);
   image.set(52, 41, red);
-  line_parametric(13, 20, 80, 40, image, white);
+  line(13, 20, 80, 40, image, white);
+  line(20, 13, 40, 80, image, red); // not drawn
+  line(80, 40, 13, 20, image, red); // some points not drawn
 
-  image.flip_vertically(); // i want to have the origin at the left bottom
-                           // corner of the image
+  image.flip_vertically();
   image.write_tga_file("output.tga");
   return 0;
 }
