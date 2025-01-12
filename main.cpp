1ef9f7085aa0324f2fb5c818780f43435564e705
lin-ion <lin-ion@naver.com>
2025-01-12 15:11:59 +0900

    Using barycentric coordinates to rasterization

--- main.cpp
+++ main.cpp
@@ -1,83 +1,50 @@
+#include <vector>
+#include <iostream>
 #include "geometry.h"
 #include "tgaimage.h"
-#include <algorithm>
-#include <array>
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor green = TGAColor(0, 255, 0, 255);
+Vec3f barycentric(Vec2i *pts, Vec2i P){;
+  Vec2i AB = Vec2i(pts[2]-pts[0]);
+  Vec2i AC = Vec2i(pts[1]-pts[0]);
+  Vec2i PA = Vec2i(pts[0]-P);
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-  bool steep = false;
-  if (std::abs(x1 - x0) < std::abs(y1 - y0)) { // transpose
-    std::swap(x0, y0);
-    std::swap(x1, y1);
-    steep = true;
-  }
+  Vec3f u = Vec3f(AB.x, AC.x, PA.x)^Vec3f(AB.y, AC.y, PA.y);
+  // u/u[2] represents barycentric coordinates
 
-  if (x0 > x1) {
-    std::swap(x0, x1);
-    std::swap(y0, y1);
-  }
+  // Vec2f _uAB = Vec2f((u.raw[0]/u.raw[2])*AB.x, (u.raw[0]/u.raw[2])*AB.y);
+  // Vec2f _vAC = Vec2f((u.raw[1]/u.raw[2])*AC.x, (u.raw[1]/u.raw[2])*AC.y);
+  // std::cout << "P ("<<pts[0].x+_uAB.x+_vAC.x<<", "<<pts[0].y+_uAB.y+_vAC.y <<")\n";
 
-  int dx = x1 - x0;
-  int dy = y1 - y0;
-  float derror2 = std::abs(dy) * 2;
-  float error2 = 0;
-  int y = y0;
-  for (int x = x0; x <= x1; x++) {
-    if (steep) { // de-transpose
-      image.set(y, x, color);
-    } else {
-      image.set(x, y, color);
-    }
-
-    error2 += derror2;
-    if (error2 > dx) {
-      y += (y1 > y0 ? 1 : -1);
-      error2 -= dx * 2;
-    } // else y is not change
-  }
+  if (std::abs(u.z)<1) return Vec3f(-1,1,1);
+  return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
 }
 
-void line(Vec2i t0, Vec2i t1, TGAImage &image, TGAColor color) {
-  line(t0.x, t0.y, t1.x, t1.y, image, color);
-}
+void triangle(Vec2i *pts, TGAImage &image, TGAColor color) {
+  Vec2i bboxmin(image.get_width()-1, image.get_height()-1);
+  Vec2i bboxmax(0, 0);
+  Vec2i clamp(image.get_width()-1, image.get_height()-1);
+  for (int i=0; i<3; i++){
+    bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+    bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
-  if (t0.y==t1.y && t0.y==t2.y) return;
-
-  if (t0.y > t1.y) std::swap(t0, t1);
-  if (t0.y > t2.y) std::swap(t0, t2);
-  if (t1.y > t2.y) std::swap(t1, t2);
-
-  int total_height = t2.y - t0.y;
-  for (int i=0; i<=total_height; i++) {
-    bool second_half = i>t1.y-t0.y || t1.y==t0.y;
-    int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
-    float alpha = (float)i / total_height;
-    float beta = (float)(i-(second_half ? t1.y-t0.y : 0)) / segment_height;
-
-    Vec2i A = t0 + (t2-t0)*alpha;
-    Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta;
-    if(A.x>B.x) std::swap(A, B);
-    for (int j=A.x; j<=B.x; j++){
-      image.set(j, t0.y+i, color);
+    bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+    bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+  }
+  Vec2i P;
+  for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++){
+    for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++){
+      Vec3f bc_screen = barycentric(pts, P);
+      if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
+      image.set(P.x, P.y, color);
     }
   }
 }
 
-int main(int argc, char **argv) {
-  TGAImage image(200, 200, TGAImage::RGB);
-
-  Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
-  Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
-  Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
-  triangle(t0[0], t0[1], t0[2], image, red);
-  triangle(t1[0], t1[1], t1[2], image, white);
-  triangle(t2[0], t2[1], t2[2], image, green);
-
-  image.flip_vertically();
-  image.write_tga_file("output.tga");
-  return 0;
+int main(int argc, char** argv) { 
+    TGAImage frame(200, 200, TGAImage::RGB); 
+    Vec2i pts[3] = {Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160)}; 
+    triangle(pts, frame, TGAColor(255, 0, 0, 255)); 
+    frame.flip_vertically(); // to place the origin in the bottom left corner of the image 
+    frame.write_tga_file("framebuffer.tga");
+    return 0; 
 }
